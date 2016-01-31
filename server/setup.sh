e6ae906e3083ed8c64e52a507f058014e000fe8e
Christopher Riley <riguy724@gmail.com>
2016-01-31 17:31:17 -0500

    Update setup docs

---
+++ server/setup.sh
@@ -0,0 +1,17 @@
+# Install necessary packages
+npm install
+composer install
+
+# Create Database
+echo "drop database test; create database test;" | mysql -u root -p -P 3306 -h localhost
+php artisan migrate:refresh --path=database/migrations
+
+# Import local data
+mysql -u root -p -P 3306 -h localhost test < server/data-import.sql
+
+# Run build, static analysis and unit tests
+node_modules/.bin/gulp
+vendor/bin/phpunit
+
+# Start the server
+php artisan serve --port=8000
